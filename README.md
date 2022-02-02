678b1db97a4d550c11bba8c430b659b52e9cd2e3
Colina Guan <cobguan@ucsc.edu>
2022-02-01 17:01:13 -0800

    README and setup routes for creating account

---
+++ README.md
@@ -0,0 +1,11 @@
+## Setting up Dev Environment
+
+Run `npm install` in this directory and the client directory.
+
+Run `npm start` in this directory; the backend api will run on localhost:5000.
+
+Run `npm start` in the client directory; the React app will run on localhost:3000.
+
+## Deployment Instructions
+
+`git push heroku main`
---
+++ client/src/app/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import Landing from './pages/Landing';
+import GetStarted from './pages/GetStarted';
+import Login from './pages/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Landing}/>
+          <Route exact path='/login' component={Login}/>
+          <Route expact path='/getstarted' component={GetStarted}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
