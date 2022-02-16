2cec4a7e941e8966a6cccba1d0f8a226d5cd29b8
Colina Guan <cobguan@ucsc.edu>
2022-02-16 10:55:01 -0800

    cleaned up authContext file

--- client/src/app/util/AuthContext.js
+++ client/src/app/util/AuthContext.js
@@ -1,70 +1,26 @@
-import React, {createContext} from 'react';
+import React, {useContext, createContext, useState} from 'react';
 
-const AuthContext = createContext({
-  user: null,
-  login: (_email, _password) => {},
-  signUp: (_email, _password) => {},
-  logout: () => {},
-});
+// initializes context
+const AuthContext = createContext();
 
 /**
  * component that provides authcontext
+ * @param {*} props things passed in
  * @return {JSX} auth provider for auth context
  */
-export function AuthProvider() {
+export function AuthProvider(props) {
   const [user, setUser] = useState();
-  const history = useHistory();
-
-  /**
-   * basic login function
-   * @param {*} email user's inputted email
-   * @param {*} _password user's inputted password
-   */
-  function login(email, _password) {
-    setLoading(true);
-
-    setUser(email);
-    history.push('/');
-  }
-
-  /**
-   * basic signup function
-   * @param {*} email user's inputted email
-   * @param {*} _password user's inputted password
-   */
-  function signup(email, _password) {
-    setLoading(true);
-
-    setUser(email);
-    history.push('/');
-  }
-
-  /**
-   * basic logout function
-   */
-  function logout() {
-    setUser(undefined);
-  }
-
-  const memoedValue = useMemo(
-      () => ({
-        user,
-        login,
-        signup,
-        logout,
-      }),
-      [user],
-  );
 
   return (
-    <AuthContext.Provider value={memoedValue}>
+    <AuthContext.Provider value={{user, setUser}}>
+      {props.children}
     </AuthContext.Provider>
   );
 }
 
 /**
  * allows other components to use auth
- * @return {context} allows other components to use
+ * @return {context} user
  */
 export default function useAuth() {
   return useContext(AuthContext);
