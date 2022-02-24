abaa1e0d8fe3084fcd482bf437fd83c6474a2cc0
Lawrence Hu <mnmrock3@gmail.com>
2022-02-24 15:48:13 -0800

    JWT is stored in cookie

--- models/user_api.js
+++ models/user_api.js
@@ -6,6 +6,12 @@ const uuid = require('uuid');
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 
+// JWT Requires
+const jwt = require('jsonwebtoken');
+// The Json file is for development purposes only
+const secrets = require('./secrets.json');
+
+
 /**
  * POSTs a user object
  * @param {*} req
@@ -19,9 +25,8 @@ exports.userPost = async (req, res) => {
   newUUID = uuid.v4();
 
   const result = await userModel.createUser(req.body, newUUID);
-  if (result == 1)
-  {
-    var user = {useremail: req.body.useremail, userid: newUUID, active: false}
+  if (result == 1) {
+    var user = { useremail: req.body.useremail, userid: newUUID, active: false }
     res.status(200).send(user);
   }
   else {
@@ -62,14 +67,13 @@ exports.userVerifyPost = async (req, res) => {
   console.log(req.body);
   const user = await userModel.getUser(req.body.useremail);
   console.log('user length = ' + user.length);
-  if (user.length < 1)
-  { 
+  if (user.length < 1) {
     res.status(401).send('Invalid username')
     return;
   }
   console.log(user);
   // eslint-disable-next-line max-len
-  bcrypt.compare(req.body.userpassword, user[0].userpassword, function(err, isMatch) {
+  bcrypt.compare(req.body.userpassword, user[0].userpassword, function (err, isMatch) {
     if (err) {
       console.log('PW error');
       throw err;
@@ -78,6 +82,23 @@ exports.userVerifyPost = async (req, res) => {
     else if (isMatch) {
       console.log('PW Match');
       delete user[0].userpassword;
+      console.log(user[0]);
+
+      // Signing a JWT Token, returned as a response
+      const accessToken = jwt.sign(
+        {
+          userid: user[0].userid,
+          useremail: user[0].useremail,
+          active: user[0].active,
+        },
+        secrets.accessToken,
+        {
+          expiresIn: '60m',
+          algorithm: 'HS256',
+        }
+      );
+      
+      res.cookie('accessToken', accessToken, { httpOnly: true });
       res.status(200).send(user[0]);
     }
     else {
@@ -85,5 +106,5 @@ exports.userVerifyPost = async (req, res) => {
       res.status(401).send('Invalid password');
     }
   })
-  
+
 };
