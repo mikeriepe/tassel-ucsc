58d2d5813b82d7d25507dcd28e68c1380ad306a1
Sneha <sneha@myhotelmanager.co.uk>
2023-03-17 10:16:14 -0700

    feat: :sparkles: First draft of Recommendation Engine
    Implement recommendation algorithm, create mock user and opp data

---
+++ models/recommendation/opp_recommendation.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# CONSTANTS
+SAMPLE_SIZE = 200
+COMMON_WORDS = [' a ', ' the ', ' and ']
+
+def clean_text(text):
+    res = str(text).lower()
+    for word in COMMON_WORDS:
+        res = res.replace(word, ' ')
+    return res
+
+# read json file into dict
+opp_json = {}
+with open("mock_opps.json", "r") as mock_opps:
+    opp_json = json.load(mock_opps)
+
+# convert json into pandas dataframe
+df = pd.DataFrame.from_dict(opp_json)
+
+# get random sample of rows to process
+# df = df.sample(n=SAMPLE_SIZE)
+
+# add target user to dataframe so can
+# be compared to opps
+userId = 'asfdgfh' # TODO: will be actual user id 
+user = {
+    'eventid': userId,
+    'eventname': None, 
+    'organization': None,
+    'description': 'Sales', # TODO: user about
+    'eventdata': 'Dynamic Assurance Associate mollit adipisicing sit in ipsum Central Tactics Specialist Central Tactics Specialist',
+}
+df = pd.concat([df, pd.DataFrame(user, index=[0])], ignore_index=True)
+
+
+# CLEANING
+
+# convert columns used to lower case and remove common words
+df['eventname'] = df['eventname'].apply(clean_text)
+df['organization'] = df['organization'].apply(clean_text)
+df['description'] = df['description'].apply(clean_text)
+df['eventdata'] = df['eventdata'].apply(clean_text)
+
+# remove irrelevant columns
+# TODO: remove other irrelevant columns from actual data
+new_df = df.drop(['eventid'],axis=1)
+
+# add 'data' column with event name, organization, description
+# and extra details combined
+new_df['data'] = new_df[new_df.columns].apply(
+    lambda x: ' '.join(x.dropna().astype(str)),
+    axis=1
+)
+
+# TODO: add user after combining data column
+
+# convert data to vector of strings to counts
+vectorizer = CountVectorizer()
+vectorized = vectorizer.fit_transform(new_df['data'])
+
+# use cosine similarity to find resemblance between
+# each row in dataset
+similarities = cosine_similarity(vectorized)
+
+# map similarity vector to profileid in original data frame
+# creates frame with profileid vertically and horizontally,
+# representing similarity between these two books
+df = pd.DataFrame(
+    similarities,
+    columns=df['eventid'],
+    index=df['eventid']
+).reset_index()
+
+# choose 10 closest opps matching user
+recs = pd.DataFrame(df.nlargest(11, userId)['eventid'])
+recs = recs[recs['eventid'] != userId]
+
+# output results
+print('CHOSEN USER: ', userId)
+print('RECOMMENDED OPPS')
+print(recs)
---
+++ models/recommendation/user_recommendation.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# CONSTANTS
+SAMPLE_SIZE = 200
+COMMON_WORDS = ['a', 'the', 'and']
+
+# read json file into dict
+user_json = {}
+with open("mock_users.json", "r") as mock_users:
+    user_json = json.load(mock_users)
+
+    # add work & volunteer experience title & description
+    # to experience list
+    for user in user_json:
+        # initialise experience string for current user
+        experience = []
+
+        # add work experience title and description
+        # to experience list
+        for item in user['experience']:
+            experience.append(item['title'])
+            experience.append(item['description'])
+            
+        # same for volunteer experience
+        for item in user['volunteeringexperience']:
+            experience.append(item['title'])
+            experience.append(item['description'])
+
+        # remove  volunteer experience,
+        del user['volunteeringexperience']
+
+        res = ' '.join(experience).lower()
+        
+        # remove common words to prevent interference
+        for word in COMMON_WORDS:
+            res = res.replace(word, '')
+        
+        # replace experience field with new combined string
+        user['experience'] = res
+        
+
+# convert json into pandas dataframe
+df = pd.DataFrame.from_dict(user_json)
+
+# get random sample of rows to process
+# df = df.sample(n=SAMPLE_SIZE)
+
+# add target opportunity to dataframe so can
+# be compared to users
+oppId = 'asfdgfh' # TODO: will be actual opp id 
+opp = {
+    'profileid': oppId,
+    'userid': None,
+    'graduationyear': None,
+    'userlocation': None,
+    'firstname': None,
+    'lastname': None,
+    'major': 'Sales',
+    'experience': 'Dynamic Assurance Associate mollit adipisicing sit in ipsum Central Tactics Specialist Central Tactics Specialist',
+    'about': 'Fundamental zero tolerance local area network',
+}
+df = pd.concat([df, pd.DataFrame(opp, index=[0])], ignore_index=True)
+
+# CLEANING
+
+# remove irrelevant columns
+new_df = df.drop(['profileid','userid','graduationyear','userlocation','firstname','lastname'],axis=1)
+
+# add 'data' column with about, experience and major
+new_df['data'] = new_df[new_df.columns[1:]].apply(
+    lambda x: ' '.join(x.dropna().astype(str)),
+    axis=1
+)
+
+# convert data to vector of strings to counts
+vectorizer = CountVectorizer()
+vectorized = vectorizer.fit_transform(new_df['data'])
+
+# use cosine similarity to find resemblance between
+# each row in dataset
+similarities = cosine_similarity(vectorized)
+
+# map similarity vector to profileid in original data frame
+# creates frame with profileid vertically and horizontally,
+# representing similarity between these two books
+df = pd.DataFrame(
+    similarities,
+    columns=df['profileid'],
+    index=df['profileid']
+).reset_index()
+
+# choose 10 closest users matching opp
+recs = pd.DataFrame(df.nlargest(11, oppId)['profileid'])
+recs = recs[recs['profileid'] != oppId]
+
+# output results
+print('CHOSEN OPP: ', oppId)
+print('RECOMMENDED USERS')
+print(recs)
